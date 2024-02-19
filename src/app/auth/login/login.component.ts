62e39e1ed6db080a9052b5fc587ea90647292d73
Dennisse Cannobbio <dcannobbio@cariola.cl>
2024-02-19 16:41:49 -0300

    Copia de Proyecto Angular AdminPro, desde otro repositorio propio, avance seccion 14

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,119 @@
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
+import { UsuarioService } from '../../services/usuario.service';
+import Swal from 'sweetalert2';
+
+// declare const gapi:any;
+declare const google: any;
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: [ './login.component.css' ]
+})
+export class LoginComponent implements OnInit {
+
+  public formSubmitted = false;
+  public auth2: any;
+
+  public loginForm = this.fb.group({
+    email: [ localStorage.getItem('email') || '' , [ Validators.required, Validators.email ] ],
+    password: ['', Validators.required ],
+    remember: [false]
+  });
+
+
+  constructor( private router: Router,
+               private fb: FormBuilder,
+               private usuarioService: UsuarioService,
+               private ngZone: NgZone ) { }
+
+  ngOnInit(): void {
+    // this.renderButton();
+  }
+
+  ngAfterViewInit(): void {
+    this.googleInit();
+  }
+
+  googleInit(){
+    google.accounts.id.initialize({
+      client_id: '804191360855-jmtnl51pdmcg9c2co925gj898864of0m.apps.googleusercontent.com',
+      callback: this.handleCredentialResponse
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("buttonDiv"),
+      { theme: "outline", size: "large" }  // customization attributes
+    );
+  }
+
+  handleCredentialResponse(response: any){
+    console.log("Encoded JWT ID token: " + response.credential);
+  }
+
+
+  login() {
+
+    this.usuarioService.login( this.loginForm.value )
+      .subscribe( resp => {
+
+        if ( this.loginForm.get('remember').value ){
+          localStorage.setItem('email', this.loginForm.get('email').value );
+        } else {
+          localStorage.removeItem('email');
+        }
+
+        // Navegar al Dashboard
+        this.router.navigateByUrl('/');
+
+      }, (err) => {
+        // Si sucede un error
+        Swal.fire('Error', err.error.msg, 'error' );
+      });
+
+  }
+
+  // renderButton() {
+  //   gapi.signin2.render('my-signin2', {
+  //     'scope': 'profile email',
+  //     'width': 240,
+  //     'height': 50,
+  //     'longtitle': true,
+  //     'theme': 'dark',
+  //   });
+
+  //   this.startApp();
+
+  // }
+
+  // async startApp() {
+
+  //   await this.usuarioService.googleInit();
+  //   this.auth2 = this.usuarioService.auth2;
+
+  //   this.attachSignin( document.getElementById('my-signin2') );
+
+  // };
+
+  // attachSignin(element) {
+
+  //   this.auth2.attachClickHandler( element, {},
+  //       (googleUser) => {
+  //           const id_token = googleUser.getAuthResponse().id_token;
+  //           // console.log(id_token);
+  //           this.usuarioService.loginGoogle( id_token )
+  //             .subscribe( resp => {
+  //               // Navegar al Dashboard
+  //               this.ngZone.run( () => {
+  //                 this.router.navigateByUrl('/');
+  //               })
+  //             });
+
+  //       }, (error) => {
+  //           alert(JSON.stringify(error, undefined, 2));
+  //       });
+  // }
+
+}
